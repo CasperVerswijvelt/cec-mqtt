10a462c836bc88910408e2100e3c1029adf63ada
Casper Verswijvelt <casperverswijvelt@gmail.com>
2022-10-26 22:12:39 +0200

    Seperate topics, simplify

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ let config = {};
 let cecClient = null;
 let mqttClient = null;
 
-const cmdQueue = { power: [] };
 const lastState = {};
-const lastPowerCmdTime = {};
-const startTime = Date.now();
 
 // Read config
 try {
@@ -24,47 +21,35 @@ console.log('Attempting to connect to MQTT server');
 mqttClient = mqtt.connect(config.mqttUrl);
 mqttClient.on('connect', () => {
   console.log('Connected to MQTT broker')
-  for (const id in config.idMap) {
-    for (const subTopic of config.subTopics) {
-      const topic = config.idMap[id].rootTopic + '/' + subTopic
-      console.log('Subscribing to ' + topic);
-      mqttClient.subscribe(topic);
-    }
+  for (const id in config.topicMap) {
+    const topic = config.topicMap[id].commandTopic
+    console.log('Subscribing to ' + topic);
+    mqttClient.subscribe(topic);
   }
 });
 mqttClient.on('message', (topic, message) => {
   console.log(' mqtt < ' + topic + ': ' + message.toString());
-  if ((Date.now() - startTime) < 5000) {
-    console.log('  Assuming MQTT update is from initial subscription, ignoring');
-    return;
-  }
   let id = 0
-  for (id in config.idMap) {
-    if (topic.indexOf(config.idMap[id].rootTopic) === 0) break;
+  for (id in config.topicMap) {
+    if (topic === config.topicMap[id].commandTopic) break;
   }
-  const subTopic = topic.split('/')[topic.split('/').length - 1];
 
-  switch (subTopic) {
-    case 'power':
-      if (
-        (
-          message.toString() === 'ON' || 
-          message.toString() === 'OFF'
-        ) && 
-        lastState[id] !== message.toString()
-      ) {
-        console.log('turning ' + id + ' ' + message.toString());
-        lastPowerCmdTime[id] = Date.now();
-        switch (message.toString()) {
-          case 'OFF':
-            cecClient.stdin.write(`standby ${id}\n`);
-            break;
-          case 'ON':
-            cecClient.stdin.write(`on ${id}\n`);
-            break;
-        }
-      }
-      break;
+  if (
+    (
+      message.toString() === 'ON' ||
+      message.toString() === 'OFF'
+    ) &&
+    lastState[id] !== message.toString()
+  ) {
+    console.log(` cecClient >  ${id}: ${message.toString()}`);
+    switch (message.toString()) {
+      case 'OFF':
+        cecClient.stdin.write(`standby ${id}\n`);
+        break;
+      case 'ON':
+        cecClient.stdin.write(`on ${id}\n`);
+        break;
+    }
   }
 });
 
@@ -75,52 +60,43 @@ cecClient.stdout.on('data', (data) => {
     .map(line => line.trim())
     .forEach(line => {
 
-    const hasPowerStatus = line.indexOf('power status: ') === 0;
+      const hasPowerStatus = line.indexOf('power status: ') === 0;
 
-    // Only log non-spammy cec client messages
-    if (line && !hasPowerStatus) console.log(` cecClient < ${line}`);
+      // Only log non-spammy cec client messages
+      if (line && !hasPowerStatus) console.log(` cecClient < ${line}`);
 
-    // Power status update
-    if (
-      config.subTopics.indexOf('power') !== -1 &&
-      hasPowerStatus
-    ) {
-      const id = cmdQueue.power.shift(); // Get the first element from the array
-      const reportedState = line.split(': ')[1];
-      let state;
-      switch (reportedState) {
-        case 'in transition from standby to on':
-        case 'on':
-          state = 'ON';
-          break;
-        case 'unknown':
-        case 'standby':
-        default:
-          state = 'OFF';
-          break;
+      // Power status update
+      if (hasPowerStatus) {
+        // TODO Get dynamic
+        const id = "0"
+        const reportedState = line.split(': ')[1];
+        let state;
+        switch (reportedState) {
+          case 'in transition from standby to on':
+          case 'on':
+            state = 'ON';
+            break;
+          case 'unknown':
+          case 'standby':
+          default:
+            state = 'OFF';
+            break;
+        }
+
+        if (lastState[id] !== state) {
+          console.log(` cecClient < ${line}`);
+          const topic = config.topicMap[id].stateTopic
+          console.log(` mqtt > ${topic}: ${state}`);
+          mqttClient.publish(topic, state, { retain: true });
+        }
+        lastState[id] = state;
       }
-
-      const timeDiff = Date.now() - (lastPowerCmdTime[id] || 0)
-
-      if (
-        lastState[id] !== state &&
-        timeDiff > config.powerCmdWaitMs
-      ) {
-        const topic = `${config.idMap[id].rootTopic}/power`
-        console.log(` mqtt > ${topic}: ${state}`);
-        mqttClient.publish(topic , state, { retain: true });
-      }
-      lastState[id] = state;
-    }
-  })
+    })
 });
 
 // Periodic power check
-if (config.subTopics.indexOf('power') !== -1) {
-  setInterval(() => {
-    Object.keys(config.idMap).forEach(async (id) => {
-      cmdQueue.power.push(id);
-      cecClient.stdin.write(`pow ${id}\n`);
-    })
-  }, config.powerCheckIntMs);
-}
+setInterval(() => {
+  Object.keys(config.topicMap).forEach(async (id) => {
+    cecClient.stdin.write(`pow ${id}\n`);
+  })
+}, config.powerCheckInterval);
